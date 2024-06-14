8497c6cad47a6b681afad98ad29e7cbeec3fed18
Remy Noel <remy.noel@blade-group.com>
2024-06-14 09:34:13 +0200

    Fix reloading not working properly.
    With an sd-notify service, systemd does not acknowledge the reloading
    and will timeout unless MONOTONIC_USEC is passed. From the
    systemd.service man page:
    
        When initiating the reload process the service is expected to reply
        with a notification message via sd_notify(3) that contains the
        "RELOADING=1" field in combination with "MONOTONIC_USEC=" set to
        the current monotonic time (i.e. CLOCK_MONOTONIC in
        clock_gettime(2)) in μs, formatted as decimal string. Once reloading
        is complete another notification message must be sent, containing
        "READY=1".
    
    We call the clock_gettime function directly as go does not provide an
    access to the system monotonic clock.
    
    This forces us to move the Reloading util to  notify_linux.

--- notify_linux.go
+++ notify_linux.go
@@ -1,9 +1,21 @@
 package sdnotify
 
 import (
+	"fmt"
 	"net"
 	"os"
 )
+
+/*
+#include <time.h>
+static unsigned long long get_nsecs(void)
+{
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    return (unsigned long long)ts.tv_sec * 1000000000UL + ts.tv_nsec;
+}
+*/
+import "C"
 
 // SdNotify sends a specified string to the systemd notification socket.
 func SdNotify(state string) error {
@@ -20,4 +32,11 @@ func SdNotify(state string) error {
 
 	_, err = conn.Write([]byte(state))
 	return err
+}
+
+// Reloading sends RELOADING=1\nMONOTONIC_USEC=<monotonic_time> to the 
+// systemd notify socket.
+func Reloading() error {
+	return SdNotify(fmt.Sprintf("RELOADING=1\nMONOTONIC_USEC=%d",
+			C.get_nsecs() / 1000))
 }
